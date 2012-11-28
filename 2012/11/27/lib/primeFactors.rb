dc34b449bae1ca96bfcab0e2fb7ee80ac66b8c72
Ross Fuhrman <rossfuhrman@gmail.com>
2012-11-27 19:03:26 -0600

    Prime Factors Kata

---
+++ 2012/11/27/lib/primeFactors.rb
@@ -0,0 +1,20 @@
+class PrimeFactors
+  def generate(input)
+    get_prime_factors(input)
+  end
+  def get_prime_factors(input)
+    i = input - 1
+    while i > 1
+      if input % i == 0 
+        prime_factors = []
+        prime_factors << input / i
+        get_prime_factors(i).each do |factor|
+          prime_factors << factor
+        end
+        return prime_factors
+      end
+      i -= 1
+    end
+    [input]
+  end
+end
